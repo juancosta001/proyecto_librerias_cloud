b4c58bffa5c952c2e039a0578857067a7393e056
juan <juanacostapereira9@gmail.com>
2025-07-03 01:44:37 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,6 @@ php artisan config:cache
 php artisan route:cache
 php artisan migrate --force || true
 
-exec php-fpm
+
 echo "🚀 Iniciando Laravel HTTP server..."
 php artisan serve --host=0.0.0.0 --port=8080
