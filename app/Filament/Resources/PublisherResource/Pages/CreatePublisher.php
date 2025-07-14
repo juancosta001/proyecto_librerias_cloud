94f9e6b210c2e626a8aad9c37c6c45a8391ece83
zZata11 <mbopicula18@gmail.com>
2025-07-13 22:32:09 -0300

    Se corrigen las redirecciones

--- app/Filament/Resources/PublisherResource/Pages/CreatePublisher.php
+++ app/Filament/Resources/PublisherResource/Pages/CreatePublisher.php
@@ -9,4 +9,12 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePublisher extends CreateRecord
 {
     protected static string $resource = PublisherResource::class;
+      public function getTitle(): string
+    {
+        return "Crear Editorial";
+    }
+    public function getRedirectUrl(): string
+    {
+        return route('filament.admin.resources.publishers.index');
+    }
 }
