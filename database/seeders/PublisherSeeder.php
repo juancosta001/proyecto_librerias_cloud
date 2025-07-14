3cc14da246aa78bed0f9e1d4f16f39c767cb26ae
zZata11 <mbopicula18@gmail.com>
2025-07-13 22:33:45 -0300

    Se registra y crea el seeder para rellenado automatico de datos

---
+++ database/seeders/PublisherSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PublisherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('publishers')->insert([
+            [
+            'name' => 'Editorial Planeta',
+            'address' => 'Calle Mayor 123, Madrid',
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],
+            [
+            'name' => 'Santillana',
+            'address' => 'Avenida de América 45, Barcelona',
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],
+            [
+            'name' => 'Alfaguara',
+            'address' => 'Gran Vía 67, Madrid',
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],
+        ]);
+    }
+}
